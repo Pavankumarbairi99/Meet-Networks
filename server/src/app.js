e5c2f4ba3f803a2321e59ef352347e6321c9afcf
Pavan Kumar <bairypawan99@gmail.com>
2025-11-16 19:25:16 +0530

    Update on app

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,9 @@ app.use("/", authrouter)
 app.use("/", profileRouter)
 app.use("/", requestRouter)
 app.use("/", userRouter)
-
+app.use("/", "hello", (req, res) => {
+    res.send("hello")
+})
 
 serverdb().then(() => {
     console.log("Conntect Sucessfully to MongoDB")
