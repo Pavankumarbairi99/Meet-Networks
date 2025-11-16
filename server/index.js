cd8de48d39b7d9e333c54ec2bcf2eaafa844e265
Pavan Kumar Bairi <135607695+Pavankumarbairi99@users.noreply.github.com>
2025-11-16 22:54:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { serverdb } = require("./src/config/dataBase");
-const User = require("./models/user");
+const User = require("./src/models/user");
 const app = express();
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
