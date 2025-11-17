74b0e20d77efe89688d5a0b655a9603fb78cbd79
Pavan Kumar Bairi <135607695+Pavankumarbairi99@users.noreply.github.com>
2025-11-17 12:09:20 +0530

    Update NavBar.jsx

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -32,8 +32,7 @@ const NavBar=()=>{
                </div>
             </div>
             
-            <label className="flex cursor-pointer gap-2">
-            
+           <div className="hidden lg:block"> <label className="flex cursor-pointer gap-2 ">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
@@ -49,14 +48,12 @@ const NavBar=()=>{
                 d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
             </svg>
 
-
             <input
                 type="checkbox"
                 value="dark"
                 className="toggle theme-controller"
                 defaultChecked
             />
-
             
             <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -70,7 +67,7 @@ const NavBar=()=>{
                 strokeLinejoin="round">
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
-            </label>
+            </label> </div>
             {user&&( 
                  <div className="flex gap-2 mx-3">
                 <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto mx-10 hidden md:block" />
